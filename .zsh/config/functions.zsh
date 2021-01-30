67de525e18f99c853c863c0ad2af8d6bc58dfe48
amoel18 <amoel18@student.sdu.dk>
2021-01-30 04:03:17 +0100

    asd

--- .zsh/config/functions.zsh
+++ .zsh/config/functions.zsh
@@ -80,3 +80,8 @@ ee() {
 sf() {
     find ~/.envs -type f | fzf | while read filename; do source $filename; done
 }
+fif() {
+    if [ ! "$#" -gt 0 ]; then echo "Need a string to search for!"; return 1; fi
+    local file
+    file="$(rga --max-count=1 --ignore-case --files-with-matches --no-messages "$*" | fzf-tmux +m --preview="rga --ignore-case --pretty --context 10 '"$*"' {}")" && echo "opening $file" && open "$file" || return 1;
+}
